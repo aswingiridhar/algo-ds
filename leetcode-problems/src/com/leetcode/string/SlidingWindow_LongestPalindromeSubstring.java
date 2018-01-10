dd061dcf154b09bf2b9ffa322b9d49bb3d312780
HariniAswin <harini.juno@gmail.com>
2018-01-09 22:51:58 -0800

    Longest Palindrom

--- leetcode-problems/src/com/leetcode/string/SlidingWindow_LongestPalindromeSubstring.java
+++ leetcode-problems/src/com/leetcode/string/SlidingWindow_LongestPalindromeSubstring.java
@@ -1,6 +1,7 @@
 package com.leetcode.string;
 
 
+
 /**
  * 
  * Problem : https://leetcode.com/problems/find-all-anagrams-in-a-string/description/
@@ -12,15 +13,31 @@ public class SlidingWindow_LongestPalindromeSubstring {
 	
 	public static void main (String [] args) {
 		
-		System.out.println("Longest Palindrome : " + longestPalindrome("cbbd").toString());
+		System.out.println("Longest Palindrome : " + longestPalindrome("ababa").toString());
 		
 	}
 	
 	public static String longestPalindrome(String s) {
-		
-		
-		
-		return "";
+	    int start = 0, end = 0;
+	    for (int i = 0; i < s.length(); i++) {
+	        int len1 = expandAroundCenter(s, i, i);
+	        int len2 = expandAroundCenter(s, i, i + 1);
+	        int len = Math.max(len1, len2);
+	        if (len > end - start) {
+	            start = i - (len - 1) / 2;
+	            end = i + len / 2;
+	        }
+	    }
+	    return s.substring(start, end + 1);
+	}
+
+	private static int expandAroundCenter(String s, int left, int right) {
+	    int L = left, R = right;
+	    while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+	        L--;
+	        R++;
+	    }
+	    return R - L - 1;
 	}
 	
 
